740881c405c82b16c2fcc3637389c0a90a219975
dogoncouch <dpersons617@gmail.com>
2017-03-08 00:47:46 -0500

    Add type function

--- marklar.py
+++ marklar.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #_    Copyright (C) 2017 Dan Persons (dpersonsdev@gmail.com)
 #_
@@ -19,55 +19,100 @@
 class MarklarError(Exception):
     pass
 
-def marklar(marklar):
+# To Do: Add 'marklar complexity' (depth)
+
+def size(marklar):
     """Returns the number of marklar in your marklar, and the average marklar size"""
     try:
-        marklar = marklar + []
-        l = 0
-        try:
-            marklar = marklar + 1 - 1
-            for m in marklar:
-                l = l + m
-            return len(marklar), l/len(marklar)
-        except TypeError:
-            for m in marklar:
-                l = l + len(m)
-            return len(marklar), l/len(marklar)
-    except TypeError: pass
-    
-    try:
         if marklar.split('\n')[1]:
-            l = 0
-            for m in marklar.split('\n'):
-                l = l + len(m)
+            l = [ 0 + len(m) for m in marklar.split('\n') ]
             return len(marklar), l/len(marklar)
-
         elif marklar.split(' ')[1]:
-            l = 0
-            for m in marklar.split(' '):
-                l = l + len(m)
+            l = [ 0 + len(m) for m in marklar.split(' ') ]
             return len(marklar), l/len(marklar)
-
         elif marklar.split('.')[1]:
-            l = 0
-            for m in marklar.split('.'):
-                l = l + len(m)
+            l = [ 0 + len(m) for m in marklar.split('.') ]
             return len(marklar), l/len(marklar)
         elif marklar.split(',')[1]:
-            l = 0
-            for m in marklar.split(','):
-                l = l + len(m)
+            l = [ 0 + len(m) for m in marklar.split(',') ]
             return len(marklar), l/len(marklar)
+        else: return len(marklar), 1
 
     except AttributeError: pass
     
     try:
+        marklar = marklar + []
+        try:
+            l = [ 0 + m for m in marklar ]
+            return len(marklar), l/len(marklar)
+        except TypeError: pass
+        try:
+            l = [ 0 + len(m) for m in marklar ]
+            return len(marklar), l/len(marklar)
+        except TypeError: pass
+    except TypeError: pass
+    try:
         marklar.update({})
-        l = 0
-        for m in marklar:
-            l = l + len(marklar[m])
+        l = [ 0 + len(marklar[m]) for m in marklar
         return len(marklar), l/len(marklar)
 
     except AttributeError: pass
     
-    raise MarklarError("Unrecognized marklar")
+    raise MarklarError('Unrecognized marklar')
+
+def type(marklar):
+    """Checks if all the marklar in your marklar are the same marklar type"""
+    try:
+        marklar = marklar + ''
+        if marklar.isalpha(): return True
+        else if marklar.isnumeric(): return True
+        else if marklar.isspace(): return True
+        else: return False
+    except TypeError: pass
+
+    try:
+        x = marklar ** 2
+        return True
+    except TypeError: pass 
+    
+    try:
+        marklar = marklar + []
+        try:
+            x = sum(m for m in marklar)
+            return True
+        except TypeError: return False
+        try:
+            x = [ ' ' + m for m in marklar ]
+            return True
+        except TypeError: return False
+        try:
+            x = [ [] + m for m in marklar ]
+            return True
+        except TypeError: return False
+        try:
+            x = [ {} + m for m in marklar ]
+            return True
+        except TypeError: return False
+    except TypeError: pass
+
+    try:
+        marklar = marklar + {}
+        try:
+            x = sum(marklar[m] for m in marklar)
+            return True
+        except TypeError: return False
+        try:
+            x = [ ' ' + marklar[m] for m in marklar ]
+            return True
+        except TypeError: return False
+        try:
+            x = [ [] + marklar[m] for m in marklar ]
+            return True
+        except TypeError: return False
+        try:
+            x = [ {} + marklar[m] for m in marklar ]
+            return True
+        except TypeError: return False
+    except TypeError: pass
+
+    raise MarklarError('Unrecognized marklar')
